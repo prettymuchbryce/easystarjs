c1440f8e5d92dbef50194b4217d4a903579d7cf4
Bryce Neal <bryceneal@CSEAL-M429649.local>
2013-06-06 15:03:40 -0700

    Add a test for avoidAdditionalPoint()

--- test/spec/easystartest.js
+++ test/spec/easystartest.js
@@ -8,22 +8,49 @@ describe("EasyStar.js", function() {
 	var map = [[1,1,0,1,1],
 		   [1,1,0,1,1],
 		   [1,1,0,1,1],
-		   [1,1,0,1,1],
+		   [1,1,1,1,1],
 		   [1,1,1,1,1]];
 
 	easyStar.setGrid(map);
 
-	easyStar.setPath(1,3,3,3);
+	easyStar.setPath(1,2,3,2);
 
 	easyStar.calculate();
 
 	function onPathFound(path) {
 		expect(path.length).toEqual(5);
 		expect(path[0].x).toEqual(1);
-		expect(path[0].y).toEqual(3);
+		expect(path[0].y).toEqual(2);
 		expect(path[2].x).toEqual(2);
+		expect(path[2].y).toEqual(3);
+	}
+  });
+
+  it("It should be able to avoid a separate point successfully.", function() {
+	var acceptableTiles = [1];
+	var easyStar = new EasyStar.js(acceptableTiles, onPathFound);
+	var map = [[1,1,0,1,1],
+		   [1,1,0,1,1],
+		   [1,1,0,1,1],
+		   [1,1,1,1,1],
+		   [1,1,1,1,1]];
+
+	easyStar.setGrid(map);
+
+	easyStar.avoidAdditionalPoint(2,3);
+
+	easyStar.setPath(1,2,3,2);
+
+	easyStar.calculate();
+
+	function onPathFound(path) {
+		expect(path.length).toEqual(7);
+		expect(path[0].x).toEqual(1);
+		expect(path[0].y).toEqual(2);
+		expect(path[2].x).toEqual(1);
 		expect(path[2].y).toEqual(4);
 	}
   });
+
 
 });
