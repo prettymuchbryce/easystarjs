a1364cb375a3833ee57a511a610767d211f99334
James Morrin <treasonx@gmail.com>
2013-11-17 06:01:43 -0500

    karma testing setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,26 @@ module.exports = function(grunt) {
         src: ['src/node.js', 'src/priorityqueue.js', 'src/instance.js', 'src/easystar.js'],
         dest: '',
       }
+    },
+    karma: {
+      options: {
+        configFile: 'karma.conf.js'
+      },
+      dev: {
+
+      },
+      coverage: {
+        preprocessors: {
+          'src/*.js': 'coverage'
+        },
+
+        browsers: ['PhantomJS'],
+        coverageReporter: {
+          type : 'html',
+          dir : 'coverage/'
+        },
+        reporters: ['coverage', 'progress']
+      }
     }
   });
 
@@ -25,5 +45,7 @@ module.exports = function(grunt) {
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-karma');
+
 
 };
