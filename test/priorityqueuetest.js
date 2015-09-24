e6b6a2237880131f2e7869230a04beed8741a2c5
Bryce Neal <brycedneal@gmail.com>
2015-09-23 20:32:57 -0700

    Fix formatting issues in priorityqueuetest.js

--- test/priorityqueuetest.js
+++ test/priorityqueuetest.js
@@ -1,32 +1,31 @@
 describe("PriorityQueue", function() {
 
-  beforeEach(function() { });
+	beforeEach(function() { });
 
-  it("It should sort objects by min value.", function() {
-	p = new EasyStar.PriorityQueue("value", EasyStar.PriorityQueue.MIN_HEAP);
-	
-	var object1 = {};
-	var object2 = {};
-	var object3 = {};
-	var object4 = {};
+	it("It should sort objects by min value.", function() {
+		p = new EasyStar.PriorityQueue("value", EasyStar.PriorityQueue.MIN_HEAP);
 
-	object1.value = 5;
-	object2.value = 10;
-	object3.value = 20;
-	object4.value = 15;
+		var object1 = {};
+		var object2 = {};
+		var object3 = {};
+		var object4 = {};
 
-	p.insert(object1);
-	p.insert(object2);
-	p.insert(object3);
-	p.insert(object4);
+		object1.value = 5;
+		object2.value = 10;
+		object3.value = 20;
+		object4.value = 15;
 
-	expect(p.shiftHighestPriorityElement()).toBe(object1);
-	expect(p.shiftHighestPriorityElement()).toBe(object2);
-  	expect(p.shiftHighestPriorityElement()).toBe(object4);
- 	expect(p.shiftHighestPriorityElement()).toBe(object3); 
-	expect(p.length).toBe(0);
+		p.insert(object1);
+		p.insert(object2);
+		p.insert(object3);
+		p.insert(object4);
 
-  });
+		expect(p.shiftHighestPriorityElement()).toBe(object1);
+		expect(p.shiftHighestPriorityElement()).toBe(object2);
+		expect(p.shiftHighestPriorityElement()).toBe(object4);
+		expect(p.shiftHighestPriorityElement()).toBe(object3); 
+		expect(p.length).toBe(0);
+	});
 
 	it("It should sort two objects by min value", function() {
 		p = new EasyStar.PriorityQueue("value", EasyStar.PriorityQueue.MIN_HEAP);
@@ -46,30 +45,29 @@ describe("PriorityQueue", function() {
 		expect(p.shiftHighestPriorityElement()).toBe(object2);
 	});
 
-  it("It should sort objects by max value.", function() {
-  	p = new EasyStar.PriorityQueue("value", EasyStar.PriorityQueue.MAX_HEAP);
-	
-	var object1 = {};
-	var object2 = {};
-	var object3 = {};
-	var object4 = {};
+	it("It should sort objects by max value.", function() {
+		p = new EasyStar.PriorityQueue("value", EasyStar.PriorityQueue.MAX_HEAP);
 
-	object1.value = 5;
-	object2.value = 10;
-	object3.value = 20;
-	object4.value = 15;
+		var object1 = {};
+		var object2 = {};
+		var object3 = {};
+		var object4 = {};
 
-	p.insert(object1);
-	p.insert(object2);
-	p.insert(object3);
-	p.insert(object4);
+		object1.value = 5;
+		object2.value = 10;
+		object3.value = 20;
+		object4.value = 15;
 
-	expect(p.shiftHighestPriorityElement()).toBe(object3);
-	expect(p.shiftHighestPriorityElement()).toBe(object4);
-  	expect(p.shiftHighestPriorityElement()).toBe(object2);
- 	expect(p.shiftHighestPriorityElement()).toBe(object1); 
-	expect(p.length).toBe(0);
+		p.insert(object1);
+		p.insert(object2);
+		p.insert(object3);
+		p.insert(object4);
 
-  });
+		expect(p.shiftHighestPriorityElement()).toBe(object3);
+		expect(p.shiftHighestPriorityElement()).toBe(object4);
+		expect(p.shiftHighestPriorityElement()).toBe(object2);
+		expect(p.shiftHighestPriorityElement()).toBe(object1); 
+		expect(p.length).toBe(0);
+	});
 
 });
