555643ce9b7a59e577443ca5be700deb8a05713d
James Clark <sbj@dimins.com>
2017-03-30 23:27:29 -0400

    Add a 1000x1000 benchmark.
    This pushes the limits of `karma-benchmarkjs-reporter`, in that it doesn't show precise results when ops take more than one second.
    
    But a test like this is important to avoid optimizing the small-grid case at the expense of the large-grid case.

--- test/easystarbenchmark.js
+++ test/easystarbenchmark.js
@@ -38,6 +38,21 @@ function setup10x10field() {
     easyStar.setAcceptableTiles([1]);
     easyStar.enableSync();
 }
+function setup1000x1000field() {
+    easyStar = new EasyStar.js();
+    var map = [];
+    for (var y = 0; y < 1000; y ++) {
+        var row = [];
+        for (var x = 0; x < 1000; x ++) {
+            row.push(1);
+        }
+        map.push(row);
+    }
+
+    easyStar.setGrid(map);
+    easyStar.setAcceptableTiles([1]);
+    easyStar.enableSync();
+}
 
 suite('EasyStar.js', function() {
     benchmark('10x10 maze no diagonals', {
@@ -102,6 +117,39 @@ suite('EasyStar.js', function() {
         },
         setup: function() {
             setup10x10field();
+            easyStar.enableDiagonals();
+            easyStar.enableCornerCutting();
+        }
+    });
+    benchmark('1000x1000 field no diagonals', {
+        fn: function() {
+            easyStar.findPath(0,0,999,999,onPathFound);
+            easyStar.calculate();
+        },
+        setup: function() {
+            setup1000x1000field();
+            easyStar.disableDiagonals();
+            easyStar.disableCornerCutting();
+        }
+    });
+    benchmark('1000x1000 field diagonals but no corner-cut', {
+        fn: function() {
+            easyStar.findPath(0,0,999,999,onPathFound);
+            easyStar.calculate();
+        },
+        setup: function() {
+            setup1000x1000field();
+            easyStar.enableDiagonals();
+            easyStar.disableCornerCutting();
+        }
+    });
+    benchmark('1000x1000 field diagonals and corner-cut', {
+        fn: function() {
+            easyStar.findPath(0,0,999,999,onPathFound);
+            easyStar.calculate();
+        },
+        setup: function() {
+            setup1000x1000field();
             easyStar.enableDiagonals();
             easyStar.enableCornerCutting();
         }
